228c2a1320b0a3278da6cab0dfe8f908215de14b
Akshat Dobriyal <akcooldobriyal@gmail.com>
2021-06-29 19:07:08 +0530

    Updates

---
+++ precache-manifest.6876b8d096e5b79080335fd076c9e1e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f35506155a6a8a7bc0c39ce97951615d",
+    "url": "/vote-now/index.html"
+  },
+  {
+    "revision": "a58af86adb9635d9e35a",
+    "url": "/vote-now/static/css/2.726cb190.chunk.css"
+  },
+  {
+    "revision": "a202f8b037ed08dedf31",
+    "url": "/vote-now/static/css/main.c9db4d1a.chunk.css"
+  },
+  {
+    "revision": "a58af86adb9635d9e35a",
+    "url": "/vote-now/static/js/2.82b2e629.chunk.js"
+  },
+  {
+    "revision": "a202f8b037ed08dedf31",
+    "url": "/vote-now/static/js/main.dafbc043.chunk.js"
+  },
+  {
+    "revision": "9bb2c8b7ac1672361f1f",
+    "url": "/vote-now/static/js/runtime-main.65c5dee2.js"
+  },
+  {
+    "revision": "a68ce5a2f2247719d91c35d6167813d4",
+    "url": "/vote-now/static/media/electionFlag.a68ce5a2.jpg"
+  }
+]);
